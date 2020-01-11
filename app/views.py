8df0994ffc4c6c4fc06bc5f83e0e9107dc8d4964
virginiah894 <virgyperry@gmail.com>
2020-01-11 15:14:38 +0300

    add vie function for display models

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project, Profile
 
 # Create your views here.
+def home(request):
+  projects= Project.objects.all()
+  return render(request,'index.html',{'projects':projects})
+

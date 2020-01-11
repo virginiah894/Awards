9f0974b7c4607483b787c6c9412a1d4bcd8b330f
virginiah894 <virgyperry@gmail.com>
2020-01-11 15:17:49 +0300

    add the model data to b e displayed

--- awards/urls.py
+++ awards/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib .auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('app.urls')),
+    path('accounts/',include('registration.backends.simple.urls')),
+    path('logout/',views.logout,{'next_page':'/'}),
 ]

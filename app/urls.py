e43e70dbf268438b10f084ba0891d40ee502cc5e
virginiah894 <virgyperry@gmail.com>
2020-01-12 16:24:01 +0300

    add paths for searching,adding,creating profile and new projects

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,17 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
 
-path('',views.home,name='home')
+path('',views.home,name='home'),
+path('search/',views.search_results,name='search_results'),
+path('project/<int:id>/',views.single_project,name ='singleProject'),
+path('profile/', views.profile , name = 'profile'),
+path('profile_update/',views.profile_update,name='update'),
+path('new_project/<int:id>/',views.add_project,name='newProject'),
+
+
 ]
 
 if settings.DEBUG:

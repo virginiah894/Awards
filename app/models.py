a990596f23e63a81686c9bb7a563c2315369903d
virginiah894 <virgyperry@gmail.com>
2020-01-14 15:31:36 +0300

    Add signals to connect user and profile

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from PIL import Image
 import datetime as dt
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import  post_save
 
 # Create your models here.
 
@@ -13,7 +15,7 @@ class Profile(models.Model):
   name = models.CharField(max_length=70)
   email = models.EmailField(null=True)
   bio = models.CharField(max_length=400)
-  profile_image = models.ImageField(upload_to='profile/',null=True)
+  profile_image = models.ImageField(upload_to='profile/',null=True, blank=True)
   user= models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile',null=True)
   
 
@@ -26,13 +28,25 @@ class Profile(models.Model):
     profiles = cls.objects.all()
     return profiles
 
+
+
+  @receiver(post_save,sender=User)
+  def create_profile(sender, instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    
+  @receiver(post_save,sender=User)
+  def save_profile(sender, instance,**kwargs):
+    instance.profile.save()
+ 
+
 class Project(models.Model):
   title = models.CharField(max_length=50)
   project_image = models.ImageField(upload_to='projects/')
   description = models.CharField(max_length=400)
   project_link = models.CharField(max_length=70)
   date=models.DateTimeField(auto_now_add=True)
-  creator = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+  user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
   profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
   
 
@@ -50,5 +64,12 @@ class Project(models.Model):
     return projects
 
 
-
+class Review(models.Model):
+    design = models.PositiveIntegerField(default=0)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    review = models.TextField(null=True)
+    usability = models.PositiveIntegerField(default=0)
+    content = models.PositiveIntegerField(default=0)
+    
 
